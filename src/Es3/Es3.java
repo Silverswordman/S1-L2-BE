964650a2d194cda9ed8dc118f1d95aa9dc21459e
Giulia Silvestrini <silvergiuly@hotmail.it>
2023-11-28 16:43:35 +0100

    while fatto circa

--- src/Es3/Es3.java
+++ src/Es3/Es3.java
@@ -1,10 +1,38 @@
 package Es3;
 
+import java.util.Scanner;
+
 public class Es3 {
 
-//    public static void
+
+
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        while (true) {
+            System.out.println("Inserisci una stringa (:q per uscire):");
+            String word = scanner.nextLine();
 
+            if (":q".equals(word)) {
+                System.out.println("Comando di uscita");
+                break;
+            }
+
+            System.out.println("Caratteri separati dalla virgola:");
+
+            for (int i = 0; i < word.length(); i++) {
+                System.out.print(word.charAt(i));
+
+                if (i < word.length() - 1) {
+                    System.out.print(",");
+                }
+            }
+
+            System.out.println();
+        }
+
+        scanner.close();
     }
+
 }
